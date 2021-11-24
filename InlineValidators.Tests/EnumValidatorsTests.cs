70760b603a0d51abb37a8b52feaa9be0f64df251
Brandyn Bayes <brandyn.bayes@gmail.com>
2021-11-23 21:13:06 -0800

    Add enum validator tests

---
+++ InlineValidators.Tests/EnumValidatorsTests.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Xunit;
+
+namespace InlineValidators.Tests;
+public class EnumValidatorsTests
+{
+    private const string ParameterName = "parameter1";
+    private const TestEnum ParameterValue = TestEnum.Value1;
+    private const TestEnum BadParameterValue = (TestEnum)100;
+    private const string CustomErrorMessage = "A custom error message goes here.";
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("\t")]
+    public void EnsureIsValidEnum_ValidatesParameterName(string parameterName)
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(() => IV.EnsureIsValidEnum(parameterName, ParameterValue));
+        Assert.Equal("parameterName", exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData((TestEnum)(-1))]
+    [InlineData((TestEnum)100)]
+    public void EnsureIsValidEnum_ThrowsForInvalidValues(TestEnum parameterValue)
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(() => IV.EnsureIsValidEnum(ParameterName, parameterValue));
+        Assert.Equal(ParameterName, exception.ParamName);
+        Assert.StartsWith("The parameter value is not valid for the enum.", exception.Message);
+    }
+
+    [Fact]
+    public void EnsureIsValidEnum_IncludesCustomMessage()
+    {
+        var exception = Assert.Throws<ArgumentOutOfRangeException>(() => IV.EnsureIsValidEnum(ParameterName, BadParameterValue, CustomErrorMessage));
+        Assert.Equal(ParameterName, exception.ParamName);
+        Assert.StartsWith(CustomErrorMessage, exception.Message);
+    }
+
+    [Fact]
+    public void EnsureIsValidEnum_ReturnsValue()
+    {
+        var returnedValue = IV.EnsureIsValidEnum(ParameterName, ParameterValue);
+        Assert.Equal(ParameterValue, returnedValue);
+    }
+
+    [Theory]
+    [InlineData(null)]
+    [InlineData("")]
+    [InlineData("\t")]
+    public void EnsureIsValidEnumAndNotDefault_ValidatesParameterName(string parameterName)
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(() => IV.EnsureIsValidEnumAndNotDefault(parameterName, ParameterValue));
+        Assert.Equal("parameterName", exception.ParamName);
+    }
+
+    [Theory]
+    [InlineData((TestEnum)(-1))]
+    [InlineData((TestEnum)100)]
+    public void EnsureIsValidEnumAndNotDefault_ThrowsForInvalidValues(TestEnum parameterValue)
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(() => IV.EnsureIsValidEnumAndNotDefault(ParameterName, parameterValue));
+        Assert.Equal(ParameterName, exception.ParamName);
+        Assert.StartsWith("The parameter value is not valid for the enum.", exception.Message);
+    }
+
+    [Theory]
+    [InlineData(default(TestEnum))]
+    public void EnsureIsValidEnumAndNotDefault_ThrowsForDefaultValue(TestEnum parameterValue)
+    {
+        var exception = Assert.ThrowsAny<ArgumentException>(() => IV.EnsureIsValidEnumAndNotDefault(ParameterName, parameterValue));
+        Assert.Equal(ParameterName, exception.ParamName);
+        Assert.StartsWith("The parameter value cannot be the default enum value.", exception.Message);
+    }
+
+    [Fact]
+    public void EnsureIsValidEnumAndNotDefault_IncludesCustomMessage()
+    {
+        var exception = Assert.Throws<ArgumentOutOfRangeException>(() => IV.EnsureIsValidEnumAndNotDefault(ParameterName, BadParameterValue, CustomErrorMessage));
+        Assert.Equal(ParameterName, exception.ParamName);
+        Assert.StartsWith(CustomErrorMessage, exception.Message);
+    }
+
+    [Fact]
+    public void EnsureIsValidEnumAndNotDefault_ReturnsValue()
+    {
+        var returnedValue = IV.EnsureIsValidEnumAndNotDefault(ParameterName, ParameterValue);
+        Assert.Equal(ParameterValue, returnedValue);
+    }
+
+    public enum TestEnum
+    {
+        DefaultValue,
+        Value1,
+    }
+}
--- InlineValidators/EnumValidators.cs
+++ InlineValidators/EnumValidators.cs
@@ -5,23 +5,17 @@ namespace InlineValidators;
 public static partial class IV
 {
     /// <summary>
-    /// Checks the provided parameter to ensure that the value is not null or the enum value is valid.
+    /// Checks the provided parameter to ensure that the value is a valid enum value.
     /// </summary>
     /// <param name="parameterName">The name of the parameter that is being checked.</param>
     /// <param name="parameterValue">The value of the parameter that is being checked.</param>
     /// <param name="message">An optional custom message to include if an exception is thrown.</param>
-    /// <returns>A non-nullable variant of the parameter value.</returns>
-    /// <exception cref="ArgumentNullException">The parameter value is null.</exception>
+    /// <returns>The validated parameter value.</returns>
     /// <exception cref="ArgumentOutOfRangeException">The parameter value is not valid for its enum.</exception>
-    public static T EnsureIsValidEnum<T>(string parameterName, T? parameterValue, string? message = null)
+    public static T EnsureIsValidEnum<T>(string parameterName, T parameterValue, string? message = null)
         where T : Enum
     {
         EnsureNotNullOrWhiteSpace(nameof(parameterName), parameterName);
-
-        if (parameterValue == null)
-        {
-            throw new ArgumentNullException(parameterName, ErrorMessageBuilder.CreateErrorMessage("Parameter can not be null.", message));
-        }
 
         if (!Enum.IsDefined(typeof(T), parameterValue))
         {
@@ -32,24 +26,23 @@ public static partial class IV
     }
 
     /// <summary>
-    /// Checks the provided parameter to ensure that the value is not null, the enum value is valid, and the value is not the default enum value.
+    /// Checks the provided parameter to ensure that the value is a valid enum value, and the value is not the default enum value.
     /// </summary>
     /// <param name="parameterName">The name of the parameter that is being checked.</param>
     /// <param name="parameterValue">The value of the parameter that is being checked.</param>
     /// <param name="message">An optional custom message to include if an exception is thrown.</param>
-    /// <returns>A non-nullable variant of the parameter value.</returns>
-    /// <exception cref="ArgumentNullException">The parameter value is null.</exception>
+    /// <returns>The validated parameter value.</returns>
     /// <exception cref="ArgumentOutOfRangeException">The parameter value is not valid for its enum, or the value is the default for the enum.</exception>
-    public static T EnsureIsValidEnumAndNotDefault<T>(string parameterName, T? parameterValue, string? message = null)
+    public static T EnsureIsValidEnumAndNotDefault<T>(string parameterName, T parameterValue, string? message = null)
         where T : Enum
     {
-        var validatedValue = EnsureIsValidEnum(parameterName, parameterValue, message);
+        EnsureIsValidEnum(parameterName, parameterValue, message);
 
-        if (validatedValue.Equals(default(T)))
+        if (parameterValue.Equals(default(T)))
         {
             throw new ArgumentOutOfRangeException(parameterName, ErrorMessageBuilder.CreateErrorMessage("The parameter value cannot be the default enum value.", message));
         }
 
-        return validatedValue;
+        return parameterValue;
     }
 }
